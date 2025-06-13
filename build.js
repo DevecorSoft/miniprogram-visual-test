645b5f4b2a86aecc0f5429c04f8ab94027095a87
zhengfeng <zhengfeng.cai@thoughtworks.com>
2025-06-13 21:09:06 +0800

    adjust bundle structure

--- build.js
+++ build.js
@@ -2,7 +2,12 @@ const fs = require('fs')
 const esbuild = require('esbuild')
 
 esbuild.build({
-  entryPoints: ['./src/vitest-matcher.ts', './src/jest-matcher.ts', './src/index.ts'],
+  entryPoints: [
+    './src/vitest-matcher.ts',
+    './src/jest-matcher.ts',
+    './src/index.ts',
+    './src/devTool/launcher.ts'
+  ],
   bundle: true,
   outdir: './dist',
   platform: 'node',
@@ -12,3 +17,6 @@ esbuild.build({
 });
 
 fs.cpSync("./src/devTool/miniprogramProject", "./dist/devTool/miniprogramProject", {recursive: true})
+fs.cpSync('LICENSE', './dist/LICENSE')
+fs.cpSync('package.json', './dist/package.json')
+fs.cpSync('README.md', './dist/README.md')
